5eefda9b29063c77a7691ba7715765b8abfec791
João Tomé <joaotf159@gmail.com>
2019-05-27 12:00:56 -0300

    delete

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,44 +1,41 @@
-import React,{ Component } from "react"
+import React, { Component } from "react";
 
-import api from "../../services/api.js"
+import api from "../../services/api.js";
 
-import "./styles.css"
+import "./styles.css";
 
-export default class Main extends Component{
-    state = {
-        structures:[]
-    }
-    
-    componentDidMount(){
-        this.loadStructures();
-    }
-    
-    loadStructures = async () => {
-        const response = await api.get('/usuario')
+export default class Main extends Component {
+  state = {
+    structures: []
+  };
 
-        this.setState({ structures : response.data });
-    }
+  componentDidMount() {
+    this.loadStructures();
+  }
 
-    
+  loadStructures = async () => {
+    const response = await api.get("/usuario");
 
-    render(){
-        
-        return ( 
-            
-            <div className="list-all">
-                {this.state.structures.map(structure => (
-                    <article key={structure._id}>
-                        <strong>{structure.title}</strong>
-                        <p>{structure.description}</p>
-                        <p>{structure.date}</p>
-                        <a href={structure.url}>Acessar</a>
+    this.setState({ structures: response.data });
+  };
 
-                    </article>
-                
-                ))}
-                
-            </div>
-            
-        );
-    }
+  excludeStructures = async id => {
+    const response = await api.delete("/usuario/" + id);
+  };
+
+  render() {
+    return (
+      <div className="list-all">
+        {this.state.structures.map(structure => (
+          <article key={structure._id}>
+            <strong>{structure.title}</strong>
+            <p>{structure.description}</p>
+            <p>{structure.date}</p>
+            <a href={structure.url}>Acessar</a>
+            <a onClick={() => this.excludeStructures(structure._id)}>Feito!</a>
+          </article>
+        ))}
+      </div>
+    );
+  }
 }
