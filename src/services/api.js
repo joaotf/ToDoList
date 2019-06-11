d40fa9df52f957e0eca2c938b099987f8003637b
João <joaotf159@gmail.com>
2019-06-10 23:19:51 -0300

    Atualização final

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
-const api = axios.create({ baseURL: "https://front-back-react.herokuapp.com/api" });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+//const api = axios.create({ baseURL: "https://front-back-react.herokuapp.com/api" });
  
 export default api;
